c9be88f58f727ecc86111ee44d54bb72c4b3916d
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-11-15 05:40:59 +0700

    init next-forge

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# Epic Story
+# next-forge
 
-Standard Ae
-React + HONO
-Coolify + Hertzner
+**Production-grade Turborepo template for Next.js apps.**
+
+[next-forge](https://github.com/haydenbleasel/next-forge) is a [Next.js](https://nextjs.org/) project boilerplate for modern web application. It is designed to be a comprehensive starting point for new apps, providing a solid, opinionated foundation with a minimal amount of configuration.
+
+Clone the repo using:
+
+```sh
+npx next-forge@latest init [my-project]
+```
+
+Then read the [docs](https://docs.next-forge.com) for more information.
+
+<a href="https://github.com/haydenbleasel/next-forge/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=haydenbleasel/next-forge" />
+</a>
+
+Made with [contrib.rocks](https://contrib.rocks).
---
+++ apps/web/middleware.ts
@@ -0,0 +1,37 @@
+import { clerkMiddleware } from '@clerk/nextjs/server';
+import arcjet, { detectBot } from '@repo/security';
+import { NextResponse } from 'next/server';
+
+export const config = {
+  // matcher tells Next.js which routes to run the middleware on. This runs the
+  // middleware on all routes except for static assets and Posthog ingest
+  matcher: ['/((?!_next/static|_next/image|ingest|favicon.ico).*)'],
+};
+
+const aj = arcjet.withRule(
+  detectBot({
+    mode: 'LIVE', // will block requests. Use "DRY_RUN" to log only
+    // Block all bots except the following
+    allow: [
+      // See https://docs.arcjet.com/bot-protection/identifying-bots
+      'CATEGORY:SEARCH_ENGINE', // Allow search engines
+      'CATEGORY:PREVIEW', // Allow preview links to show OG images
+      'CATEGORY:MONITOR', // Allow uptime monitoring services
+    ],
+  })
+);
+
+export default clerkMiddleware(async (_auth, request) => {
+  const decision = await aj.protect(request);
+
+  if (
+    // If this deny comes from a bot rule then block the request. You can
+    // customize this logic to fit your needs e.g. changing the status code.
+    decision.isDenied() &&
+    decision.reason.isBot()
+  ) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 403 });
+  }
+
+  return NextResponse.next();
+});
